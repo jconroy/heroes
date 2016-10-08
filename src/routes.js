a43e8c6d8e9f449062f08a3efd8a7bdae1efb1f8
Jason Conroy <jason@findingsimple.com>
2016-10-08 22:08:19 +1100

    Fix HeroPage route

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
-import HeroPage from './components/heroPage';
+import HeroPage from './components/HeroPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
